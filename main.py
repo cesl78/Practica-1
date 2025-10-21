c984bb55226b3e01de5bfd7caef93b4251a217c5
Cesl78 <cesluisvs78@gmail.com>
2025-10-21 19:42:05 +0100

    show video

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import cv2
 
-VIDEO_CAPTURE = cv2.VideoCapture("C:\Users\cesar\Desktop\UNI\2025-2026\2025\FSI\Trabajos\Radar\tráfico01.mp4")
-import numpy as np
-
-cap = VIDEO_CAPTURE
+cap = cv2.VideoCapture(r"C:\Users\cesar\Desktop\UNI\2025-2026\2025\FSI\Trabajos\Radar\tráfico01.mp4")
 
 while (1):
 
@@ -11,12 +8,7 @@ while (1):
     _, frame = cap.read()
     frame = cv2.resize(frame, (400, 300))
 
-
-    # Calculation of Sobelx
-    sobel = cv2.Sobel(frame, cv2.CV_8U, 1, 1, ksize=3)
-
     cv2.imshow('original', frame)
-    cv2.imshow('sobel', sobel)
 
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
